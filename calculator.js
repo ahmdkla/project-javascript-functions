edccee68b48f550dfd5dbca78bd6ed8752129307
Ahmad Rafiqul A'la <ahmadakla48@gmail.com>
2019-11-05 16:29:19 +0700

    Add Calculator Function

---
+++ calculator.js
@@ -0,0 +1,39 @@
+function Addition() {
+    operand1 = prompt('Input number 1 :');
+    operand2 = prompt('Input number 2 :');
+
+    alert(parseInt(operand1) + parseInt(operand2));
+    console.log(parseInt(operand1) + parseInt(operand2));
+}
+
+function Substitution() {
+    operand1 = prompt('Input number 1 :');
+    operand2 = prompt('Input number 2 :');
+
+    alert(parseInt(operand1) - parseInt(operand2));
+    console.log(parseInt(operand1) - parseInt(operand2));
+}
+
+function Multiplication() {
+    operand1 = prompt('Input number 1 :');
+    operand2 = prompt('Input number 2 :');
+
+    alert(parseInt(operand1) * parseInt(operand2));
+    console.log(parseInt(operand1) * parseInt(operand2));
+}
+
+function Division() {
+    operand1 = prompt('Input number 1 :');
+    operand2 = prompt('Input number 2 :');
+
+    alert(parseInt(operand1) / parseInt(operand2));
+    console.log(parseInt(operand1) / parseInt(operand2));
+}
+
+function Mods() {
+    operand1 = prompt('Input number 1 :');
+    operand2 = prompt('Input number 2 :');
+
+    alert(parseInt(operand1) % parseInt(operand2));
+    console.log(parseInt(operand1) % parseInt(operand2));
+}
